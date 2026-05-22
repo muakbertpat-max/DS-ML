08e739a8ba841bff2639a616b3608d99f60aaf24
muakbertpat-max <muakbert.pat@gmail.com>
2026-05-22 15:42:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ st.write("##### Day 1: การจัดการข้อมูลพื้น
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
-elif st.button("การเปลี่ยนแปลงข้อมูล")
+elif st.button("การทำความสะอาด"):
     st.switch_page("pages/clean_app_muak.py")
