4b52ed3158e56934ec8fc9dc1e07569d1a78fccf
muakbertpat-max <muakbert.pat@gmail.com>
2026-06-03 15:25:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,5 @@ elif st.button("😘การทำความสะอาดและวิเ
     st.switch_page("pages/clean_customers.py")    
 elif st.button("😁การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
+elif st.button("😁การวิเคราะห์ข้อมูลเชิงสำรวจ"):
+    st.switch_page("pages/EDA_app.py")
