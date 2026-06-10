7c2de3218b0b0e9d85754ceb702bb14f9f56e26b
muakbertpat-max <muakbert.pat@gmail.com>
2026-06-10 13:33:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,5 @@ elif st.button("😁การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
 elif st.button("😁การวิเคราะห์ข้อมูลเชิงสำรวจ"):
     st.switch_page("pages/EDA_app.py")
+elif st.button("😁clustering segment"):
+    st.switch_page("pages/clustering_segment.py")
